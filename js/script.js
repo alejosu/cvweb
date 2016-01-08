5bd8b6acd63f8ffc80e7b1900d4cd8017323eefa
Manuel Alejandro Suarez <alejosu@gmail.com>
2016-01-08 13:02:41 -0500

    ajustes botones idiomas

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
-$( "#js-espanol" ).click(function() {
+$( "js-espanol" ).click(function() {
   $(".english").css("display","none");
   $(".espanol").css("display",("block"));
   );
 });
 
-$( "#js-english" ).click(function() {
+$( "js-english" ).click(function() {
   $(".espanol").css("display","none");
   $(".english").css("display",("block"));
   );
