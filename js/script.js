54d7bc27534393a0d9e585913cb4f5a9eec128c7
Manuel Alejandro Suarez <alejosu@gmail.com>
2016-01-08 12:53:45 -0500

    js para idiomas y estilos

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$( "#js-espanol" ).click(function() {
+  $(".english").css("display","none");
+  $(".espanol").css("display",("block"));
+  );
+});
+
+$( "#js-english" ).click(function() {
+  $(".espanol").css("display","none");
+  $(".english").css("display",("block"));
+  );
+});
